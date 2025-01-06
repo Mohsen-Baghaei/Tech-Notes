03456c7812aa6a60e8bf7f89fa76ed045f3d3347
MOHSEN BAGHAEI <mohsen.bhi99@gmail.com>
2025-01-06 16:45:35 +0330

    create cors

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,12 +4,20 @@ const path = require("path");
 
 const { logger } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
+const corsOptions = require("./config/corsOptions");
+
+const cookeParser = require("cookie-parser");
+const cors = require("cors");
 
 const PORT = process.env.PROT || 3500;
 
 app.use(logger);
 
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
+app.use(cookeParser);
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
