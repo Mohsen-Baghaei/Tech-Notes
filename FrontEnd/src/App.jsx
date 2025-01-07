b87865aef73ffb561d0d7dedcc4c0d164debf926
Mohsen Baghaei <mohsen.bhi99@gmail.com>
2025-01-07 15:24:26 +0330

    working on basics of front end

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,33 @@
-function App() {
-  return <></>;
-}
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./featurs/auth/Login";
+import DashLayout from "./components/DashLayout";
+import Welcome from "./featurs/auth/Welcome";
+import NotesList from "./featurs/notes/NotesList";
+import UsersList from "./featurs/users/UsersList";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+
+        <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+        </Route>
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
